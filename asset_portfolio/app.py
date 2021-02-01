4e77aba7eb6b4c15a0e7f23bd8582ceea61b6b1a
Andres Berejnoi <andresberejnoi@gmail.com>
2021-02-01 14:40:14 -0500

    removed old home route code

--- asset_portfolio/app.py
+++ asset_portfolio/app.py
@@ -75,49 +75,5 @@ def home():
         db.session.commit()
     return render_template("home.html")
 
-@app.route("/",methods=["GET", "POST"])
-def OLD_home():
-    if request.form:
-        try:
-            print(request.form)
-            tuples = command_engine(request.form.get("transactions"))   #transactions is how the text input field is called in the html page for this endpoint
-        except:
-            return "Something went horribly wrong, but I don't know what"
-        
-        for inst,master_trans_dict in tuples:
-            if inst.lower() == 'add':
-                for symbol in master_trans_dict:
-                    SYMBOL_object = Security.query.filter(Security.symbol==symbol).first()     #without .first() the return is a query object
-                    if SYMBOL_object is None:
-                        SYMBOL_object = Security(symbol)
-                        db.session.add(SYMBOL_object)
-                        db.session.commit()
-                    print(f"------> {SYMBOL_object}")
-                    trans_dict = master_trans_dict[symbol]
-
-                    transactions = trans_dict.get('transactions',None)
-                    flags        = trans_dict['flags']
-                    for idx,trans in enumerate(transactions):
-                        num_shares = trans[0]
-                        cost_basis = trans[1]
-
-                        flags_list = flags[idx][1]   #the second element is always a list of flags, such as ['-d','-robinhood', etc]
-
-                        if '-d' in flags_list:
-                            is_dividend = True
-                        else:
-                            is_dividend = False
-                        
-                        #Create the Transaction Object to insert into the database
-                        TRANS_object = Transaction(symbol, num_shares, cost_basis, is_dividend)
-                        SYMBOL_object.transactions.append(TRANS_object)
-                        db.session.add(TRANS_object)
-        
-        db.session.commit()
-
-
-
-    return render_template("home.html")
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
