eaa163fe5175967feb487dd57548e74b03a43ed5
Andres Berejnoi <andresberejnoi@gmail.com>
2021-02-01 23:58:41 -0500

    completed Transactions form

--- asset_portfolio/forms.py
+++ asset_portfolio/forms.py
@@ -1,17 +1,18 @@
 from wtforms import (SubmitField, BooleanField, StringField, 
                     PasswordField, validators)
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 
-class TransactionsForm(Form):
-    pass 
+class TransactionsForm(FlaskForm):
+    transactions_str = StringField('Enter string here: (i.e. aapl 0.4 200 -t 10:48, msft 0.5 228 -dt 2020-05-30 13:55)',[validators.DataRequired()]) 
+    submit = SubmitField('Submit')
 
-class CryptoWalletForm(Form):
-    crypto = StringField('Crypto Currency (i.e. BTC, ETH, XRP, LTC, etc)',[validators.DataRequired()])
+class CryptoWalletForm(FlaskForm):
+    symbol = StringField('Crypto Currency (i.e. BTC, ETH, XRP, LTC, etc)',[validators.DataRequired()])
     nickname = StringField('Wallet Nickname')
     address = StringField('Public Address',[validators.DataRequired()])
     submit = SubmitField('Submit')
 
-class RegisterBrokerForm(Form):
+class RegisterBrokerForm(FlaskForm):
     name = StringField('Enter Broker database name (type something short)',[validators.DataRequired()])
     website = StringField("Broker's Website")
     submit = SubmitField('Submit')
